f5113553c705a8b90cc1f46ed6ebe22675f8b77c
lubomirp <accounts@pastorek.org>
2023-03-07 18:53:23 +0100

    change lazy

--- init.lua
+++ init.lua
@@ -2,6 +2,18 @@
 --config neovim--
 -----------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
